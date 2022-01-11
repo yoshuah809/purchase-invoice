2ebc3d10e777a71379741495178402a9b6420be1
Josue Garcia <yoshuah809@gmail.com>
2022-01-11 16:49:10 -0600

    created no-privileges templates

--- app/bases/urls.py
+++ app/bases/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from bases.views import Home
+from bases.views import Home, HomeWithNoPrivileges
 
 urlpatterns = [
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'), name='login'),
 
     path('logout/', auth_views.LogoutView.as_view(template_name='bases/login.html'), name='logout'),
+
+    path('no_privileges/', HomeWithNoPrivileges.as_view(), name='no_privileges'),
 ]
--- app/bases/views.py
+++ app/bases/views.py
@@ -8,3 +8,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
     login_url='bases:login'
+
+
+
+class HomeWithNoPrivileges(LoginRequiredMixin, generic.TemplateView):
+    login_url = "bases:login"
+    template_name="bases/no_privileges.html"
