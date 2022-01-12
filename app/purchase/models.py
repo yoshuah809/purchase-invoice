9b8c9e4d74428b5dbba4c9b082e619b23cd63677
Josue Garcia <yoshuah809@gmail.com>
2022-01-12 09:38:19 -0600

    added purchase header and details models

--- app/purchase/models.py
+++ app/purchase/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from bases.models import ModelClass
+from inv.models import Product
 
 # Create your models here.
 
@@ -18,3 +19,52 @@ class Provider(ModelClass):
     self.name = self.name.upper()   
     self.address = self.address.upper()  
     super(Provider, self).save() 
+
+
+class PurchaseHeader(ModelClass):
+  purchase_date=models.DateField(null=True,blank=True)
+  observation=models.TextField(blank=True,null=True)
+  invoice_no=models.CharField(max_length=100)
+  invoice_date=models.DateField()
+  sub_total=models.FloatField(default=0)
+  discount=models.FloatField(default=0)
+  total=models.FloatField(default=0)
+
+  provider=models.ForeignKey(Provider,on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return '{}'.format(self.observation)
+
+  def save(self):
+    self.observation = self.observation.upper()
+    if self.sub_total == None  or self.discount == None:
+      self.sub_total = 0
+      self.discount = 0
+          
+    self.total = self.sub_total - self.discount
+    super(PurchaseHeader,self).save()
+
+  class Meta:
+    verbose_name="Purchase Header"    
+
+
+class PurchaseDetail(ModelClass):
+    purchase=models.ForeignKey(PurchaseHeader,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.BigIntegerField(default=0)
+    provider_price=models.FloatField(default=0)
+    sub_total=models.FloatField(default=0)
+    discount=models.FloatField(default=0)
+    total=models.FloatField(default=0)
+    cost=models.FloatField(default=0)
+
+    def __str__(self):
+      return '{}'.format(self.product)
+
+    def save(self):
+      self.sub_total = float(float(int(self.quantity)) * float(self.provider_price))
+      self.total = self.sub_total - float(self.discount)
+      super(PurchaseDetail, self).save()
+    
+    class Meta:
+      verbose_name="Purchase Detail"
--- app/purchase/views.py
+++ app/purchase/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import generic
 
 from django.urls import reverse_lazy
+from bases.views import NoProvileges
 from purchase.forms import ProviderForm
 
 from purchase.models import Provider
@@ -12,27 +14,29 @@ from purchase.models import Provider
 # Create your views here.
 
 
-class ProviderView(LoginRequiredMixin, generic.ListView):
+class ProviderView(NoProvileges, generic.ListView):
+    permission_required='inv.view_provider'
     model = Provider
     template_name = "purchase/provider_list.html"
     context_object_name= "obj"
-    login_url='bases:login'
+    
 
-
-class NewProvider(LoginRequiredMixin, generic.CreateView):
-    model=Provider
+class NewProvider(NoProvileges, generic.CreateView):
+    permission_required='inv.new_provider'
+    model=Provider    
     template_name = 'purchase/provider_form.html'
     context_object_name='obj'
     form_class=ProviderForm
     success_url=reverse_lazy("purchase:provider_list")
-    login_url='bases:login'
+    
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
 
-class EditProvider(LoginRequiredMixin, generic.UpdateView):
+class EditProvider(NoProvileges, generic.UpdateView):
+    permission_required='inv.edit_provider'
     model=Provider
     template_name = 'purchase/provider_form.html'
     context_object_name='obj'
@@ -44,7 +48,8 @@ class EditProvider(LoginRequiredMixin, generic.UpdateView):
         form.instance.modified_by = self.request.user.id
         return super().form_valid(form)
 
-
+@login_required(login_url='/login/')
+@permission_required('inv.change_provider', login_url='bases:no_privileges')
 def deactivate_provider(request, id):
     provider = Provider.objects.filter(pk=id).first()
     context={}
